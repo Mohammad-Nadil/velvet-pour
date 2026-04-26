15548e614b53fa031e3795f5ff4798105ac7fb22
Mohammed Hossainur Rashid Nadil <mdhossain.dev@gmail.com>
2026-04-26 16:47:59 +0600

    item hidden

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./Components/Hero";
 import Menu from "./Components/Menu";
 import About from "./Components/About";
 import Art from "./Components/Art";
-import Item from "./Components/Item";
+// import Item from "./Components/Item";
 import Footer from "./Components/Footer";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
@@ -18,7 +18,7 @@ const App = () => {
       <Menu />
       <About />
       <Art />
-      <Item />
+      {/* <Item /> */}
       <Footer />
     </div>
   );
